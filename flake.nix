c002edf86ea799c8fe7fa21ea6df0760c4285ec9
Jack W. <git@jack.cab>
2023-11-06 16:07:51 -0500

    refactor: :boom: better typings + metainfo + use Adw.Application instead of Gtk.Application
    going insane again

--- flake.nix
+++ flake.nix
@@ -17,9 +17,9 @@
         lib = pkgs.lib;
       in
       {
-        default = pkgs.stdenvNoCC.mkDerivation {
+        default = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
           pname = "methodsave";
-          version = "0.1.0";
+          version = "0.0.1";
           src = self;
           nativeBuildInputs = [ pkgs.gjs pkgs.gobject-introspection pkgs.wrapGAppsHook pkgs.blueprint-compiler ];
           buildInputs = [
@@ -33,23 +33,43 @@
           buildPhase = ''
             ln --symbolic ${troll} ./src/troll
             mkdir $out
-            gjs --module ${troll}/gjspack/bin/gjspack --appid=uk.cetera.Methodsave --import-map=./src/import_map.json --blueprint-compiler=${lib.getExe pkgs.blueprint-compiler} ./src/main.js $out/bin
+            export version="${finalAttrs.version}"
+            export comments="${finalAttrs.meta.description}"
+            export homepage="${finalAttrs.meta.homepage}"
+            substituteAllInPlace ./src/main.js
+            gjs --module ${troll}/gjspack/bin/gjspack ${lib.cli.toGNUCommandLineShell {} {
+              appid = "uk.cetera.Methodsave";
+              import-map = "./src/import_map.json";
+              resource-root="./src";
+            }} ./src/main.js $out/bin
             sed --in-place "1s/.*/#!${lib.strings.escape ["/"] (lib.getExe pkgs.gjs)} --module/" $out/bin/uk.cetera.Methodsave
+            substituteAllInPlace ./share/dbus-1/services/uk.cetera.Methodsave.service
             cp --recursive ./share $out/share
           '';
-          meta.mainProgram = "uk.cetera.Methodsave";
+          meta = {
+            mainProgram = "uk.cetera.Methodsave";
+            license = lib.licenses.gpl3Plus;
+            description = "Archive and ask questions about everything you Save";
+            homepage = "https://cetera.uk/";
+            longDescription = builtins.readFile ./README.md;
+            changelog = "https://github.com/mollersuite/methodsave/releases/tag/v${finalAttrs.version}";
+          };
           desktopItem = pkgs.makeDesktopItem {
             name = "uk.cetera.Methodsave"; # Filename, not display name
             icon = "uk.cetera.Methodsave";
             exec = "uk.cetera.Methodsave %u";
             desktopName = "Methodsave";
             genericName = "Bookmark Manager";
-            comment = "All together now.";
-            # dbusActivatable = true;
+            categories = [ "ArtificialIntelligence" "GNOME" ];
+            keywords = [ "Etcetera" ];
+            comment = finalAttrs.meta.description;
+            # TODO
+            dbusActivatable = true;
+            startupNotify = true;
           };
 
           postInstall = "cp -r $desktopItem/* $out";
-        };
+        });
       };
   };
 }
--- src/main.js
+++ src/main.js
@@ -1,35 +1,72 @@
+/// <reference types="gjs/dom" />
 import GObject from "gi://GObject"
 import Gio from "gi://Gio"
 import Gtk from "gi://Gtk?version=4.0"
 import Adw from "gi://Adw?version=1"
-import { programArgs as ARGV, programInvocationName as BINARY } from "system"
+import GLib from "gi://GLib"
+import { programArgs, programInvocationName } from "system"
 import { build } from "troll"
 import "troll/globals.js"
 import Interface from "./window.blp" assert { type: "uri" }
 import "./icons/about-symbolic.svg" assert { type: "icon" }
-import "./icons/adw-external-link-symbolic.svg" assert { type: "icon" }
+GLib.set_application_name("Methodsave")
 
 /** @type {<Type>(arr: Type[]) => Type} */
 const random = arr => arr[Math.floor(Math.random() * arr.length)]
 
 const Methodsave = GObject.registerClass(
 	{},
-	class Methodsave extends Gtk.Application {
+	class Methodsave extends Adw.Application {
 		constructor() {
 			super({
 				application_id: "uk.cetera.Methodsave",
-				flags: Gio.ApplicationFlags.FLAGS_NONE,
+				flags: Gio.ApplicationFlags.DEFAULT_FLAGS,
 			})
 		}
 
-		// https://stackoverflow.com/questions/44230814/invalid-object-type-when-using-a-custom-widget-in-gtk-builder-ui-file#comment99706251_44230815
-		// Invoke `typeof` on all the components we use in Blueprint to workaround "Invalid object type" error
 		vfunc_startup() {
 			super.vfunc_startup()
-			typeof Adw.ApplicationWindow
-			typeof Adw.HeaderBar
-			typeof Adw.Clamp
-			typeof Adw.StatusPage
+			// https://gjs.guide/guides/gio/dbus.html#gaction, TODO: Use actions instead
+			// see also https://developer.gnome.org/documentation/tutorials/search-provider.html
+			// also https://github.com/search?q=%22parameter_type%3A+new+GLib.%22&type=code, i need to know how to do this right
+			Gio.DBusExportedObject.wrapJSObject(
+				`<node>
+							<interface name="org.gnome.Shell.SearchProvider2">
+
+								<method name="GetInitialResultSet">
+								<arg type="as" name="terms" direction="in" />
+								<arg type="as" name="results" direction="out" />
+								</method>
+
+								<method name="GetSubsearchResultSet">
+								<arg type="as" name="previous_results" direction="in" />
+								<arg type="as" name="terms" direction="in" />
+								<arg type="as" name="results" direction="out" />
+								</method>
+
+								<method name="GetResultMetas">
+								<arg type="as" name="identifiers" direction="in" />
+								<arg type="aa{sv}" name="metas" direction="out" />
+								</method>
+
+								<method name="ActivateResult">
+								<arg type="s" name="identifier" direction="in" />
+								<arg type="as" name="terms" direction="in" />
+								<arg type="u" name="timestamp" direction="in" />
+								</method>
+
+								<method name="LaunchSearch">
+								<arg type="as" name="terms" direction="in" />
+								<arg type="u" name="timestamp" direction="in" />
+								</method>
+
+							</interface>
+							</node>`,
+				{}
+			).export(
+				Gio.DBus.session,
+				"/org/gnome/Shell/SearchProvider2/uk_cetera_Methodsave"
+			)
 		}
 
 		vfunc_activate() {
@@ -38,39 +75,57 @@ const Methodsave = GObject.registerClass(
 			/*** @type {{window: Gtk.ApplicationWindow, search: Gtk.SearchEntry}} */
 			const { window, search } = build(Interface, {
 				about: () => {
-					/**
-					 * I have to reassign the type because there's no typings for libadwaita
-					 * @type {Gtk.Window}
-					 */
 					const about = new Adw.AboutWindow({
+						comments: "@comments@",
+						version: "@version@",
+						website: "@homepage@",
 						application_name: "Methodsave",
 						application_icon: "uk.cetera.Methodsave",
-						comments: "All together now",
 						developer_name: "Etcetera Development",
 						license_type: Gtk.License.GPL_3_0,
-						website: "https://github.com/mollersuite/methodsave",
 						issue_url: "https://github.com/mollersuite/methodsave/issues",
 						support_url: "https://cetera.uk/discord",
+						developers: [
+							"Auxnos https://github.com/auxnos",
+							"Bongo https://github.com/poopyhead121",
+							"Cluster https://cluster.coolviruses.download",
+							"Jack https://jack.cab",
+							"Malice https://maliciousmeaning.dev",
+							"mau https://github.com/maumaumaumaumaumau",
+							"Nick http://github.com/nickk431",
+							"split https://me.fyle.uk",
+							"Unlinkability https://charlie.land",
+							"Wavi https://fur.dev",
+							// "jade https://auuub.is-a.dev", // Uncomment when Jade learns JavaScript
+						],
+						designers: [
+							"Jack https://jack.cab",
+							"split https://me.fyle.uk",
+							// Method's Chat mode was motivated by Dot being nonfree (both price and freedom), iOS-only, and containing off-device language models.
+							// We realized Etcetera could compete here due to Methodsave's standardized JSON-LD formats. What if the LLM could simply output JSON-LD?
+							"Jason Yuan (indirectly) https://jasonyuan.design",
+						],
+						documenters: ["Jack https://jack.cab"],
 					})
 					about.set_transient_for(window)
 					about.present()
-				},
+				}
 			})
-			search.placeholderText = `Search for ${random([
+			search.placeholder_text = `Search for ${random([
 				"ideas",
 				"thoughts",
 				"articles",
 				"bookmarks",
 				"posts",
 				"quotes",
-				"snippets"
+				"snippets",
 			])}...`
 			window.set_application(this)
 			window.set_icon_name("uk.cetera.Methodsave")
 			window.present()
-			window.connect("close-request", (win) => win.run_dispose())
+			window.connect("close-request", win => win.run_dispose())
 		}
 	}
 )
 
-new Methodsave().run([BINARY].concat(ARGV))
+new Methodsave().run([programInvocationName].concat(programArgs))
